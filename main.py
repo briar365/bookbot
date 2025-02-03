5eeaf1df37b4d2b55ec76a5972d59eee703810e1
briar365 <briar.holloman.17@gmail.com>
2025-02-02 19:37:56 -0500

    added a letter count

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ def main():
         book_path = "books/frankenstein.txt"
         text = get_book_text(book_path)
         num_words = get_num_words(text)
-        print(f"{num_words} words found in the document")
+        lower_text = lowered_string(text)
+        letter = input("Enter the letter you would like to count: ")
+        count = lower_text.count(letter)
+        print(f"'{letter}': {count}")
 
 
 def get_num_words(text):
         words = text.split()
         return len(words)
-
+def lowered_string(text):
+        lower_string = text.lower()
+        return lower_string
 
 def get_book_text(path):
     with open(path) as f:
